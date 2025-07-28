7fa896113430355f27d2c230b29cb27a78bc9926
Hassan Rahman <hr-awesome@hotmail.com>
2025-07-28 12:21:46 -0500

    First commit

---
+++ src/main/java/com/w3bd3vm4n/tm01_agenda_api/Tm01AgendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.w3bd3vm4n.tm01_agenda_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tm01AgendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tm01AgendaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w3bd3vm4n/tm01_agenda_api/Tm01AgendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w3bd3vm4n.tm01_agenda_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tm01AgendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
