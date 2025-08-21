0bbd6b9d4e7ce7eeafdace90a20e20223af54e3c
Hassan Rahman <hr-awesome@hotmail.com>
2025-08-21 14:52:09 -0500

    Add configuration to allow the CORS

---
+++ src/main/java/com/w3bd3vm4n/tm01_agenda_api/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.w3bd3vm4n.tm01_agenda_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000", "http://192.168.100.165:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
